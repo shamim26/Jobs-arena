3695659d65b7e617f55840c344b1d202e2c0af44
shamim26 <shamimreja101@gmail.com>
2023-04-10 23:35:02 +0600

    filter feature added sucessfully

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
-import CommonBanner from '../CommonBanner/CommonBanner';
-import { useLoaderData } from 'react-router-dom';
-import AppliedJob from './AppliedJob';
+import React, { useEffect, useState } from "react";
+import CommonBanner from "../CommonBanner/CommonBanner";
+import { useLoaderData } from "react-router-dom";
+import AppliedJob from "./AppliedJob";
 
 const AppliedJobs = () => {
-    const data = useLoaderData();
-    return (
-        <div>
-            <div>
-                <CommonBanner>Applied Jobs</CommonBanner>
-            </div>
-            <div className='container w-9/12 mx-auto'>
-                <select className='bg-[#F4F4F4] rounded-lg outline-none px-4 py-3 block mt-24 ml-auto' name="sort" id="">
-                    <option value="">Filter By</option>
-                    <option value="remote">Remote</option>
-                    <option value="onsite">Onsite</option>
-                </select>
-                {
-                    data.map(job => <AppliedJob key={job.id} job ={job}></AppliedJob>)
-                }
-            </div>
-        </div>
-    );
+  const data = useLoaderData();
+  const [storedJobs, setStoredJobs] = useState(data);
+  const [renderJobs, setRenderJobs] = useState([]);
+
+  useEffect(() => {
+    setRenderJobs(storedJobs);
+  }, [storedJobs]);
+  const handleSort = (selected) => {
+    if (selected === "") {
+      setRenderJobs(data);
+    } else {
+      const sorted = storedJobs.filter(
+        (job) => job.remoteOrOnsite === selected
+      );
+      setRenderJobs(sorted);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <CommonBanner>Applied Jobs</CommonBanner>
+      </div>
+      <div className="container w-9/12 mx-auto">
+        <select
+          onChange={(e) => handleSort(e.target.value)}
+          className="bg-[#F4F4F4] rounded-lg outline-none px-4 py-3 block mt-24 ml-auto"
+          name="sort"
+          id=""
+        >
+          <option value="" selected disabled>
+            Filter By
+          </option>
+          <option value="Remote">Remote</option>
+          <option value="Onsite">Onsite</option>
+        </select>
+        {renderJobs.map((job) => (
+          <AppliedJob key={job.id} job={job}></AppliedJob>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AppliedJobs;
